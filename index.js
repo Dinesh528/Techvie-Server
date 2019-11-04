15485d0d445ef5e31ddc39e0513117debba883e2
Dinesh <petadineshkumar97@gmail.com>
2019-11-04 22:53:50 +0530

    commit

--- index.js
+++ index.js
@@ -30,14 +30,6 @@ app.use('/api',Connections);
 
 mongoose.Promise = global.Promise;
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
